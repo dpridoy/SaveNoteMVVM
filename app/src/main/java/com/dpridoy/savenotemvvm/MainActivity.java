a159b05c8eb21a39df534f64ae46a45375366578
Nasirul Haque Ridoy <dpridoy@gmail.com>
2019-10-13 12:54:18 +0600

    fifth commit creating RecyclerView

--- app/src/main/java/com/dpridoy/savenotemvvm/MainActivity.java
+++ app/src/main/java/com/dpridoy/savenotemvvm/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,12 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView=findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter=new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                //update RecyclerView
-                Toast.makeText(MainActivity.this,"On Changed",Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
