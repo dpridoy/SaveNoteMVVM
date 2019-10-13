9ae6feaa541b1355d9910475f2eafa6a145f699f
Nasirul Haque Ridoy <dpridoy@gmail.com>
2019-10-13 12:00:21 +0600

    fourth commit creating View Model

--- app/src/main/java/com/dpridoy/savenotemvvm/MainActivity.java
+++ app/src/main/java/com/dpridoy/savenotemvvm/MainActivity.java
@@ -1,14 +1,31 @@
 package com.dpridoy.savenotemvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this,"On Changed",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
