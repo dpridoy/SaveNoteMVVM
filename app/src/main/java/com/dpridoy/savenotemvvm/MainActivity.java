26057f69f0421083c3c2e8902984dff13f1831dd
Nasirul Haque Ridoy <dpridoy@gmail.com>
2019-10-20 19:04:36 +0600

    sixth commit create AddNoteActivity

--- app/src/main/java/com/dpridoy/savenotemvvm/MainActivity.java
+++ app/src/main/java/com/dpridoy/savenotemvvm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dpridoy.savenotemvvm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,19 +8,32 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int ADD_NOTE_REQUEST=1;
     private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddNote=findViewById(R.id.button_add_note);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,AddNoteActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
 
         RecyclerView recyclerView=findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -35,5 +49,23 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setNotes(notes);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==ADD_NOTE_REQUEST && resultCode==RESULT_OK){
+            String title=data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description=data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority=data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+
+            Note note=new Note(title,description,priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this,"Note saved",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this,"Note not saved",Toast.LENGTH_SHORT).show();
+        }
     }
 }
